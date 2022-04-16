8d6f6fab1fa529c00801987852825b914b1fbff2
Adroaldo <hygino@msn.com>
2022-04-16 15:40:29 -0300

    Utilizando useState para mudar o estado de um componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { LoggedIn } from './state/LoggedIn';
 
 function App() {
   return (
     <div className="App">
-      <h1>Desestruturando props e exportando types</h1>
+      <h1>Utilizando useState para mudar o estado de um componente</h1>
+      <LoggedIn/>
     </div>
   );
 }
