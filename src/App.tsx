760b336c307400490708ed98ef9331ef91b2bad4
Adroaldo <hygino@msn.com>
2022-04-16 13:17:39 -0300

    Adicionando atributos as props children e tornando atributos opcionais

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
 import './App.css';
 import Greet from './components/Greet';
-import Person from './components/Person';
-import PersonList from './components/PersonList';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import { Status } from './components/Status';
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne'
-  }
-
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne'
-    },
-    {
-      first: 'Clark',
-      last: 'Kent'
-    },
-    {
-      first: 'Princess',
-      last: 'Diana'
-    }
-  ]
-
   return (
     <div className="App">
-      <h1>Olá mundo</h1>
-      <Greet name='Godofredo' messageCount={10} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList}/>
+      <Status status='loading' />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>The oscar goes to Leonardo DiCaprio</Heading>
+      </Oscar>
+      <Greet name='Vishwas' isLoggedIn={true} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,17 +1,19 @@
 type GreetProps = {
     name: string;
-    messageCount: number;
+    messageCount?: number;
     isLoggedIn: boolean;
 }
 
 const Greet = (props: GreetProps) => {
+
+    const { messageCount = 0 } = props;
     return (
         <div>
             <h2>
                 {
-                    props.isLoggedIn 
-                    ? `Bem vindo ${props.name} você tem ${props.messageCount} mensagens não lidas!` 
-                    : 'Bem vindo visitante!'
+                    props.isLoggedIn
+                        ? `Bem vindo ${props.name} você tem ${messageCount} mensagens não lidas!`
+                        : 'Bem vindo visitante!'
                 }
             </h2>
         </div>
