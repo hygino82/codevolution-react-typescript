bd59edc1aa20fbf9622f0b2bd4c8875875e89bfb
Adroaldo <hygino@msn.com>
2022-04-16 12:03:14 -0300

    Adicionando listas as props

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,11 +1,19 @@
 type GreetProps = {
     name: string;
+    messageCount: number;
+    isLoggedIn: boolean;
 }
 
 const Greet = (props: GreetProps) => {
     return (
         <div>
-            <h2>Bem vindo {props.name}</h2>
+            <h2>
+                {
+                    props.isLoggedIn 
+                    ? `Bem vindo ${props.name} você tem ${props.messageCount} mensagens não lidas!` 
+                    : 'Bem vindo visitante!'
+                }
+            </h2>
         </div>
     );
 }
